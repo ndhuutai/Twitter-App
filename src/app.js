36946ed47b0c667019c46705ba5a9c620243af52
ndhuutai <ndhuutai1@gmail.com>
2019-03-16 08:47:14 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import HomePage from './components/HomePage';
+
+
+ReactDOM.render(<HomePage/>, document.getElementById('app'));
---
+++ src/components/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => (
+	<div className="alert alert-primary">
+		HELLO WORLD!
+	</div>
+);
+
+export default HomePage;
