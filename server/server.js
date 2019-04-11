ef0d8d2a214c438fd42b85a086dbbd0ff2be6d3a
ndhuutai <ndhuutai1@gmail.com>
2019-04-11 01:50:40 -0700

    convert to async await for server

--- server/server.js
+++ server/server.js
@@ -33,32 +33,42 @@ app.use(bodyParser.json());
 app.use(express.static(publicPath));
 
 
-app.get('/searchTweets', (req,res) => {
-	axios.get('https://api.twitter.com/1.1/search/tweets.json', {
-		headers: {
-			'Content-Type': 'application/json',
-			'Accept': 'application/json',
-			Authorization: `Bearer ${token}`
-		},
-		params: {
-			...req.query
-		}
-	}).then(response => res.send(response.data))
-		.catch(err => res.send(err.response.data));
+app.get('/searchTweets', async (req,res) => {
+	try {
+		const { data } = await axios.get('https://api.twitter.com/1.1/search/tweets.json', {
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+				Authorization: `Bearer ${token}`
+			},
+			params: {
+				...req.query
+			}
+		});
+
+		res.send(data);
+	} catch (e) {
+		res.send(e.response.data);
+	}
 });
 
-app.get('/searchUser', (req, res) => {
-	axios.get('https://api.twitter.com/1.1/users/lookup.json', {
-		headers: {
-			'Content-Type': 'application/json',
-			'Accept': 'application/json',
-			Authorization: `Bearer ${token}`
-		},
-		params: {
-			...req.query
-		}
-	}).then(response => res.send(response.data))
-		.catch(err => res.send(err.response.data));
+app.get('/searchUser', async (req, res) => {
+	try {
+		const { data }= await axios.get('https://api.twitter.com/1.1/users/lookup.json', {
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+				Authorization: `Bearer ${token}`
+			},
+			params: {
+				...req.query
+			}
+		});
+
+		res.send(data);
+	} catch (e) {
+		res.send(e.response.data);
+	}
 });
 
 app.get('*', (req, res) => {
